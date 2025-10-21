60c2d7c1a7537d43e88f39b86b33fbb042d5e148
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-21 10:25:18 +0700

    menambahkan post

--- index.js
+++ index.js
@@ -41,3 +41,24 @@ app.get('/api/mahasiswa', (req, res) => {
         res.json(results);
     });
 });
+
+app.post('/api/mahasiswa', (req, res) => {
+    const { nama, nim, kelas, prodi } = req.body;
+
+    if (!nama || !nim || !kelas || !prodi) {
+        return res.status(400).json({ message: 'nama, nim, kelas, and prodi wajib diisi' });
+    }
+
+    db.query(
+        'INSERT INTO mahasiswa (nama, nim, kelas, prodi) VALUES (?, ?, ?, ?)',
+        [nama, nim, kelas, prodi],
+        (err, result) => {
+            if (err) {
+                console.error;
+                return res.status(500).json({ message: 'Database error'});
+            }
+
+            res.status(201).json({ message: 'User berhasil ditambahkan', id: result.insertId });
+        }
+    );
+});
