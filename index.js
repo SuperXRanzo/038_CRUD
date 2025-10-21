8ac712838cfaa3d967e20639f8feb6edae13da34
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-21 10:31:49 +0700

    menambahkan put

--- index.js
+++ index.js
@@ -62,3 +62,19 @@ app.post('/api/mahasiswa', (req, res) => {
         }
     );
 });
+
+app.put('/api/mahasiswa/:id', (req, res) => {
+    const userId = req.params;
+    const { nama, nim, kelas, prodi } = req.body;
+    db.query(
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ?, prodi = ? WHERE id = ?',
+        [nama, nim, kelas, prodi, userId], 
+        (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+            res.json({ message: 'User berhasil diperbarui' });
+        }
+    );
+});
